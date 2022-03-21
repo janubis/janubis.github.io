68b7555d985a67ead16150f3d93f516373077e4a
janubis <r.javkhlan@gmail.com>
2022-03-21 18:24:49 +0800

    yooh

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+module.exports = {
+  env: {
+    PUBLIC_URL: "https://janubis.github.io",
+    assetPrefix: "./",
+  },
+};
